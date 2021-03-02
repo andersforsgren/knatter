80097aa07f609701d1ee3b5af824142d01d74867
Anders Forsgren <anders.forsgren@gmail.com>
2021-03-02 21:31:11 +0100

    Replace AudioSwitcher with CoreAudio due to memory leak. Removes some functionality

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 Knatter
 =======
 
-Windows application to  mute keys when typing. Built with [AudioSwitcher](https://github.com/xenolightning/AudioSwitcher/).  Inspired by UnClack for MacOS.
+Windows application to  mute keys when typing. Built with [CoreAudio](https://github.com/morphx666/CoreAudio). Inspired by UnClack for MacOS.
 
 NOTE: Work in progress
 
-
-Prerequisites
-=============
-
-.NET Runtime 4.6.1 or later (Comes preinstalled on Win10 1511 and later)
+Prerequisites: 
+.NET Runtime 4.7.2
 
 Contributing
 ============
---
+++ Source/Knatter.Core/Audio/DeviceManager.cs
@@ -0,0 +1,25 @@
+﻿using CoreAudio;
+using System.Linq;
+
+namespace Knatter.Core.Audio
+{
+   public class DeviceManager
+   {
+      private CoreAudioDevice[] devices;
+      private readonly MMDeviceEnumerator deviceEnumerator;
+
+      public DeviceManager()
+      {
+         this.deviceEnumerator = new MMDeviceEnumerator();
+         LoadDevices();
+      }
+
+      // TODO: Handle device events changing
+      internal CoreAudioDevice[] Devices => devices;
+
+      private void LoadDevices()
+      {
+         this.devices = deviceEnumerator.EnumerateAudioEndPoints(EDataFlow.eCapture, DEVICE_STATE.DEVICE_STATE_ACTIVE).Select(mmd => new CoreAudioDevice(mmd)).ToArray();
+      }
+   }
+}
